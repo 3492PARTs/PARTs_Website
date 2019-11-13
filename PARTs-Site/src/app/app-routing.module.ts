ebc95e3f44027d15d8c984d67975a485a7eaa7e5
Brandon Duke <bmduke1997@gmail.com>
2019-11-13 16:54:59 -0500

    i think i'm happy with the nav for now

--- PARTs-Site/src/app/app-routing.module.ts
+++ PARTs-Site/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/webpages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ScoutFieldComponent } from './components/webpages/scouting/scout-field/scout-field.component';
+
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'scout-field', component: ScoutFieldComponent }
 ];
 
 @NgModule({
