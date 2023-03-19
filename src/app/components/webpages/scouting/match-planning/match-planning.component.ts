8d467da251245fceec97736d04ed5bdad8791ebd
Brandon <brandon@bduke.dev>
2023-03-19 19:28:05 -0400

    match planning

---
+++ src/app/components/webpages/scouting/match-planning/match-planning.component.ts
@@ -0,0 +1,96 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/services/general.service';
+import { CompetitionLevel } from '../scout-admin/scout-admin.component';
+import { Team } from '../scout-field/scout-field.component';
+
+@Component({
+  selector: 'app-match-planning',
+  templateUrl: './match-planning.component.html',
+  styleUrls: ['./match-planning.component.scss']
+})
+export class MatchPlanningComponent implements OnInit {
+  initData = new Init();
+
+  matchesTableCols: object[] = [
+    { PropertyName: 'match_number', ColLabel: 'Match' },
+    { PropertyName: 'time', ColLabel: 'Time' },
+    { PropertyName: 'red_one_id', ColLabel: 'Red One' },
+    { PropertyName: 'red_two_id', ColLabel: 'Red Two' },
+    { PropertyName: 'red_three_id', ColLabel: 'Red Three' },
+    { PropertyName: 'blue_one_id', ColLabel: 'Blue One' },
+    { PropertyName: 'blue_two_id', ColLabel: 'Blue Two' },
+    { PropertyName: 'blue_three_id', ColLabel: 'Blue Three' },
+  ];
+
+  constructor(private gs: GeneralService, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.init();
+  }
+
+  init(): void {
+    this.gs.incrementOutstandingCalls();
+    this.http.get(
+      'scouting/match-planning/init/'
+    ).subscribe(
+      {
+        next: (result: any) => {
+          if (this.gs.checkResponse(result)) {
+            this.initData = (result as Init);
+          }
+        },
+        error: (err: any) => {
+          console.log('error', err);
+        },
+        complete: () => {
+          this.gs.decrementOutstandingCalls();
+        }
+      }
+    );
+  }
+
+  planMatch(match: Match): void {
+    this.gs.incrementOutstandingCalls();
+    this.http.get(
+      'scouting/match-planning/plan-match/'
+    ).subscribe(
+      {
+        next: (result: any) => {
+          if (this.gs.checkResponse(result)) {
+            this.initData = (result as Init);
+          }
+        },
+        error: (err: any) => {
+          console.log('error', err);
+        },
+        complete: () => {
+          this.gs.decrementOutstandingCalls();
+        }
+      }
+    );
+  }
+}
+
+export class Match {
+  match_id!: string;
+  match_number!: number;
+  event!: Event;
+  red_one_id!: Team | number;
+  red_two_id!: Team | number;
+  red_three_id!: Team | number;
+  blue_one_id!: Team | number;
+  blue_two_id!: Team | number;
+  blue_three_id!: Team | number;
+  red_score!: number;
+  blue_score!: number;
+  comp_level!: string | CompetitionLevel;
+  time!: Date;
+  void_ind!: string;
+
+}
+
+export class Init {
+  event!: Event | null;
+  matches: Match[] = [];
+}
