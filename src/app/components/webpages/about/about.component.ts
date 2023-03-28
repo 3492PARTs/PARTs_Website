90df2055cfb34a35cda759893b62baf66ba42e92
Brandon <brandon@bduke.dev>
2023-03-28 18:44:58 -0400

    about

--- src/app/components/webpages/about/about.component.ts
+++ src/app/components/webpages/about/about.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   bot2011ModalVisible = false;
+  bot2012ModalVisible = false;
+  bot2013ModalVisible = false;
+  bot2014ModalVisible = false;
+  bot2015ModalVisible = false;
+  bot2016ModalVisible = false;
+  bot2017ModalVisible = false;
+  bot2018ModalVisible = false;
+  bot2019ModalVisible = false;
+  bot2020ModalVisible = false;
+  bot2022ModalVisible = false;
 
   constructor() { }
 
