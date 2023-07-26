de2f063baf42c9efb240cbc0a170175b61ad2c9b
Brandon <brandon@bduke.dev>
2023-07-26 17:28:37 -0400

    display items

--- src/app/components/webpages/sponsoring/sponsor-shop/sponsor-shop.component.ts
+++ src/app/components/webpages/sponsoring/sponsor-shop/sponsor-shop.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthCallStates, AuthService } from 'src/app/services/auth.service';
+import { GeneralService } from 'src/app/services/general.service';
+import { Item } from '../../admin/admin.component';
 
 @Component({
   selector: 'app-sponsor-shop',
   templateUrl: './sponsor-shop.component.html',
   styleUrls: ['./sponsor-shop.component.scss']
 })
-export class SponsorShopComponent {
+export class SponsorShopComponent implements OnInit {
+  items: Item[] = [];
 
+  constructor(private gs: GeneralService, private http: HttpClient, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.authInFlight.subscribe(r => r === AuthCallStates.comp ? this.getItems() : null);
+  }
+
+
+  getItems(): void {
+    this.gs.incrementOutstandingCalls();
+    this.http.get(
+      'sponsoring/get-items/'
+    ).subscribe(
+      {
+        next: (result: any) => {
+          if (this.gs.checkResponse(result)) {
+            this.items = result as Item[];
+          }
+        },
+        error: (err: any) => {
+          console.log('error', err);
+        },
+        complete: () => {
+          this.gs.decrementOutstandingCalls();
+        }
+      }
+    );
+  }
+
+  previewImage(link: string, id: string): void {
+    this.gs.previewImage(link, id);
+  }
 }
