efb2ba1dfdb3d7523aba0204166b47fb5b86adaa
Brandon Duke <brandon@bduke.dev>
2023-09-19 17:20:14 -0400

    update config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   version: 'v3',
-  baseUrl: 'https://parts.bduke.dev/',
-  backupBaseUrl: 'https://parts-webapi.herokuapp.com/',
+  baseUrl: 'https://api.parts3492.org/',
+  backupBaseUrl: '',
   tokenString: '8fa1792e-ce4b-11ed-afa1-0242ac120002',
   loggedInHereBefore: '2cf049f8-ce4c-11ed-afa1-0242ac120002',
   userSettings: 'db7591f0-3e7b-4a92-99e3-1ffba52f1010'
