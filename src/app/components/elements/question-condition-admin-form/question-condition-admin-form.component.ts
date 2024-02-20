644a271a79bbb72c1c0860ba4252e0195c081869
Brandon Duke <brandon@bduke.dev>
2024-02-20 14:51:09 -0500

    fix new form

--- src/app/components/elements/question-condition-admin-form/question-condition-admin-form.component.ts
+++ src/app/components/elements/question-condition-admin-form/question-condition-admin-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { QuestionCondition } from '../../webpages/scouting/scout-admin/scout-admin.component';
 import { Question } from '../question-admin-form/question-admin-form.component';
 import { GeneralService, RetMessage } from 'src/app/services/general.service';
@@ -10,32 +10,34 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./question-condition-admin-form.component.scss']
 })
 export class QuestionConditionAdminFormComponent implements OnInit {
-  fieldQuestions: Question[] = [];
-  fieldQuestionConditions: QuestionCondition[] = [];
-  fieldQuestionConditionModalVisible = false;
-  activeFieldQuestionCondition = new QuestionCondition();
-  fieldQuestionConditionsTableCols: object[] = [
+  @Input() FormType = '';
+
+  questions: Question[] = [];
+  questionConditions: QuestionCondition[] = [];
+  questionConditionModalVisible = false;
+  activeQuestionCondition = new QuestionCondition();
+  questionConditionsTableCols: object[] = [
     { PropertyName: 'question_from.display_value', ColLabel: 'Question From' },
     { PropertyName: 'condition', ColLabel: 'Condition' },
     { PropertyName: 'question_to.display_value', ColLabel: 'Question To' },
     { PropertyName: 'active', ColLabel: 'Active' },
   ];
-  fieldQuestionConditionQuestionFromList: Question[] = [];
-  fieldQuestionConditionQuestionToList: Question[] = [];
+  questionConditionQuestionFromList: Question[] = [];
+  questionConditionQuestionToList: Question[] = [];
 
   constructor(private gs: GeneralService, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getFieldQuestionConditions();
-    this.getFieldQuestionConditions();
+    this.getQuestions();
+    this.getQuestionConditions();
   }
 
-  getScoutFieldQuestions(): void {
+  getQuestions(): void {
     this.gs.incrementOutstandingCalls();
     this.http.get(
       'form/get-questions/', {
       params: {
-        form_typ: 'field',
+        form_typ: this.FormType,
         active: 'y'
       }
     }
@@ -43,9 +45,9 @@ export class QuestionConditionAdminFormComponent implements OnInit {
       {
         next: (result: any) => {
           if (this.gs.checkResponse(result)) {
-            this.fieldQuestions = result as Question[];
-            this.buildFieldQuestionConditionFromLists();
-            this.buildFieldQuestionConditionToLists();
+            this.questions = result as Question[];
+            this.buildQuestionConditionFromLists();
+            this.buildQuestionConditionToLists();
           }
         },
         error: (err: any) => {
@@ -60,20 +62,20 @@ export class QuestionConditionAdminFormComponent implements OnInit {
     );
   }
 
-  getFieldQuestionConditions(): void {
+  getQuestionConditions(): void {
     this.gs.incrementOutstandingCalls();
     this.http.get(
       'form/question-condition/', {
       params: {
-        form_typ: 'field'
+        form_typ: this.FormType
       }
     }
     ).subscribe(
       {
         next: (result: any) => {
           if (this.gs.checkResponse(result)) {
-            console.log(result);
-            this.fieldQuestionConditions = result as QuestionCondition[];
+            //console.log(result);
+            this.questionConditions = result as QuestionCondition[];
           }
         },
         error: (err: any) => {
@@ -88,53 +90,52 @@ export class QuestionConditionAdminFormComponent implements OnInit {
     );
   }
 
-  showFieldQuestionConditionModal(qc?: QuestionCondition) {
-    this.fieldQuestionConditionModalVisible = true;
-    this.activeFieldQuestionCondition = qc ? this.gs.cloneObject(qc) : new QuestionCondition();
-    this.buildFieldQuestionConditionFromLists();
-    this.buildFieldQuestionConditionToLists();
+  showQuestionConditionModal(qc?: QuestionCondition) {
+    this.questionConditionModalVisible = true;
+    this.activeQuestionCondition = qc ? this.gs.cloneObject(qc) : new QuestionCondition();
+    this.buildQuestionConditionFromLists();
+    this.buildQuestionConditionToLists();
   }
 
-  buildFieldQuestionConditionFromLists(): void {
-    this.fieldQuestionConditionQuestionFromList = [];
+  buildQuestionConditionFromLists(): void {
+    this.questionConditionQuestionFromList = [];
 
-
-    this.fieldQuestions.forEach(q => {
+    this.questions.forEach(q => {
       let match = false;
-      this.fieldQuestionConditions.forEach(qc => {
+      this.questionConditions.forEach(qc => {
         if ([qc.question_to.question_id].includes(q.question_id))
           match = true
       });
 
-      if (this.activeFieldQuestionCondition.question_to &&
-        !this.gs.strNoE(this.activeFieldQuestionCondition.question_to.question_id) &&
-        this.activeFieldQuestionCondition.question_to.question_id === q.question_id)
+      if (this.activeQuestionCondition.question_to &&
+        !this.gs.strNoE(this.activeQuestionCondition.question_to.question_id) &&
+        this.activeQuestionCondition.question_to.question_id === q.question_id)
         match = true;
 
       if (!match)
-        this.fieldQuestionConditionQuestionFromList.push(q);
+        this.questionConditionQuestionFromList.push(q);
     });
   }
 
-  buildFieldQuestionConditionToLists(): void {
-    this.fieldQuestionConditionQuestionToList = [];
+  buildQuestionConditionToLists(): void {
+    this.questionConditionQuestionToList = [];
 
-    this.fieldQuestions.forEach(q => {
+    this.questions.forEach(q => {
       let match = false;
-      this.fieldQuestionConditions.forEach(qc => {
+      this.questionConditions.forEach(qc => {
         if ([qc.question_from.question_id, qc.question_to.question_id].includes(q.question_id)) match = true
       });
 
-      if (this.activeFieldQuestionCondition.question_from &&
-        !this.gs.strNoE(this.activeFieldQuestionCondition.question_from.question_id) &&
-        this.activeFieldQuestionCondition.question_from.question_id === q.question_id) match = true;
+      if (this.activeQuestionCondition.question_from &&
+        !this.gs.strNoE(this.activeQuestionCondition.question_from.question_id) &&
+        this.activeQuestionCondition.question_from.question_id === q.question_id) match = true;
 
-      if (this.activeFieldQuestionCondition.question_to &&
-        !this.gs.strNoE(this.activeFieldQuestionCondition.question_to.question_id) &&
-        this.activeFieldQuestionCondition.question_to.question_id === q.question_id) match = false;
+      if (this.activeQuestionCondition.question_to &&
+        !this.gs.strNoE(this.activeQuestionCondition.question_to.question_id) &&
+        this.activeQuestionCondition.question_to.question_id === q.question_id) match = false;
 
       if (!match)
-        this.fieldQuestionConditionQuestionToList.push(q);
+        this.questionConditionQuestionToList.push(q);
     });
   }
 
@@ -148,15 +149,15 @@ export class QuestionConditionAdminFormComponent implements OnInit {
   saveQuestionCondition(): void {
     this.gs.incrementOutstandingCalls();
     this.http.post(
-      'form/question-condition/', this.activeFieldQuestionCondition
+      'form/question-condition/', this.activeQuestionCondition
     ).subscribe(
       {
         next: (result: any) => {
           if (this.gs.checkResponse(result)) {
             this.gs.addBanner({ message: (result as RetMessage).retMessage, severity: 1, time: 3500 });
-            this.activeFieldQuestionCondition = new QuestionCondition();
-            this.fieldQuestionConditionModalVisible = false;
-            this.getFieldQuestionConditions();
+            this.activeQuestionCondition = new QuestionCondition();
+            this.questionConditionModalVisible = false;
+            this.getQuestionConditions();
           }
         },
         error: (err: any) => {
