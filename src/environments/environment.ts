d76a5233c8c2681536cee4e77d949c75c17bfac8
Brandon <brandon@bduke.dev>
2024-02-21 20:35:40 -0500

    notif icons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   backupBaseUrl: 'http://127.0.0.1:8000/',
   tokenString: '8fa1792e-ce4b-11ed-afa1-0242ac120402',
   loggedInHereBefore: '2cf049f8-ce4c-11ed-afa1-0242ac124002',
-  userSettings: 'db7591f0-3e7b-4a92-99e3-1ffba52f1010'
+  userSettings: 'db7591f0-3e7b-4a92-99e3-1ffba52f1017'
 };
 
 /*
