1ee80a0ff21b6679a0391c9cf187295650e3b43b
Brandon <brandon@bduke.dev>
2024-03-10 22:00:01 -0400

    install new slider

--- src/app/components/webpages/home/home.component.ts
+++ src/app/components/webpages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { AppSize, GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gs: GeneralService) { }
 
   ngOnInit() {
+    this.resizeCSSSlider();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.resizeCSSSlider();
+  }
+
+  resizeCSSSlider(): void {
     const appHeader = document.getElementById('site-header') || new HTMLElement();
-    const slider = document.getElementById('cssSlider') || new HTMLElement();
-    slider.style.maxHeight = 'calc( 100vh - ' + (appHeader.offsetHeight || 0) + 'px)';
+    const slider = document.getElementById('cssSliderWrapper') || new HTMLElement();
+    if (this.gs.getAppSize() >= AppSize.LG) {
+      slider.style.height = 'calc( 100vh - ' + (appHeader.offsetHeight || 0) + 'px)';
+      slider.style.paddingBottom = 'unset';
+    }
+    else {
+      slider.style.paddingBottom = 'calc((100% * 1365) / 2048)';
+      slider.style.height = 'unset';
+    }
   }
 
 }
