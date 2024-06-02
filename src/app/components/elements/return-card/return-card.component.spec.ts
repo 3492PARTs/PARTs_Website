ce43557e331c49c4b727eaae8566eaf772973ee9
Brandon <brandon@bduke.dev>
2024-06-02 18:44:22 -0400

    restyle media

---
+++ src/app/components/elements/return-card/return-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReturnCardComponent } from './return-card.component';
+
+describe('ReturnCardComponent', () => {
+  let component: ReturnCardComponent;
+  let fixture: ComponentFixture<ReturnCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ReturnCardComponent]
+    });
+    fixture = TestBed.createComponent(ReturnCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
