a59fef4c08a0639fab73d6404006296866ab0ab7
Brandon Duke <brandon@bduke.dev>
2024-06-05 10:25:14 -0400

    footer updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '2024.05.31',
+  version: '2024.06.05',
   baseUrl: 'https://parts.bduke.dev/',
   backupBaseUrl: 'https://parts-webapi.herokuapp.com/',
   tokenString: '8fa1792e-ce4b-11ed-afa1-0242ac120002',
