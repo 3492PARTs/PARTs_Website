4bbff9fe1ef5a9bf2b726611a72e6639d701631a
Brandon Duke <brandon@bduke.dev>
2024-11-01 11:41:43 -0400

    length of message

--- src/app/components/webpages/admin/team-contact-form/team-contact-form.component.ts
+++ src/app/components/webpages/admin/team-contact-form/team-contact-form.component.ts
@@ -14,8 +14,11 @@ export class TeamContactFormComponent {
   teamContactResponsesCols = [
     { PropertyName: 'response_id', ColLabel: 'ID' },
     { PropertyName: 'questionanswer_set[0].answer', ColLabel: 'Name' },
-    { PropertyName: 'questionanswer_set[3].answer', ColLabel: 'Message' },
+    { PropertyName: 'questionanswer_set[3].answer', ColLabel: 'Message', Type: 'function', ColValueFn: this.truncateMessage },
     { PropertyName: 'time', ColLabel: 'Time' },
   ];
 
+  private truncateMessage(s: string): string {
+    return `${s.substring(0, s.length < 100 ? s.length : 100).trim()}${s.length >= 100 ? '...' : ''}`;
+  }
 }
