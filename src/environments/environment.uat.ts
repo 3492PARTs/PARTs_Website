e9ecb2a760bfe252973454416d8150ba7f5066e2
Brandon <brandon@bduke.dev>
2024-11-03 17:12:39 -0500

    remove tag

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -7,5 +7,5 @@ export const environment = {
     loggedInHereBefore: '2cf049f8-ce4c-11ed-afa1-0242ac120002',
     userSettings: 'db7591f0-3e7b-4a92-99e3-1ffba52f1010',
     rememberMe: '25b39b10-01bf-4140-ace9-1c3646681867',
-    environment: "$BRANCH"
+    environment: "BRANCH"
 };
