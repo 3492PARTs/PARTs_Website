898488c6f68a36df660cd50834fd2cd8033bd0f5
Brandon <brandon@bduke.dev>
2024-11-05 19:44:57 -0500

    fis gtag and page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import { ButtonRibbonComponent } from './components/atoms/button-ribbon/button-r
 import { ButtonComponent } from './components/atoms/button/button.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
+declare const gtag: Function;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -21,16 +23,16 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService: AuthService, public router: Router, public gs: GeneralService, @Inject(DOCUMENT) private document: Document) {
+  constructor(private authService: AuthService, public router: Router, public gs: GeneralService, @Inject(DOCUMENT) private document: Document, private route: ActivatedRoute) {
     // subscribe to router events and send page views to Google Analytics
     this.router.events.subscribe(event => {
       if (environment.production && event instanceof NavigationEnd) {
         const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
-        /*gtag('event', 'page_view', {
+        gtag('event', 'page_view', {
           page_title: title,
           page_path: event.urlAfterRedirects,
           page_location: this.document.location.href
-        })*/
+        })
       }
     });
 
@@ -55,9 +57,15 @@ export class AppComponent implements OnInit {
 
   getTitle(state: RouterState, parent: ActivatedRoute): string[] {
     const data = [];
-    if (parent && parent.snapshot.data && parent.snapshot.data['title']) {
-      data.push(parent.snapshot.data['title']);
+
+    const routeTitleSymbol = Object.getOwnPropertySymbols(parent.snapshot.data).find(
+      (symbol) => symbol.toString() === 'Symbol(RouteTitle)'
+    ) || '';
+
+    if (parent && parent.snapshot.data && parent.snapshot.data[routeTitleSymbol]) {
+      data.push(parent.snapshot.data[routeTitleSymbol]);
     }
+
     if (state && parent && parent.firstChild) {
       data.push(...this.getTitle(state, parent.firstChild));
     }
