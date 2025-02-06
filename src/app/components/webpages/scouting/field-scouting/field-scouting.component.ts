c02cdf39717e2be171fbd668693e78affbff8cbd
Brandon <brandon@bduke.dev>
2025-02-05 20:07:26 -0500

    fix auto time

--- src/app/components/webpages/scouting/field-scouting/field-scouting.component.ts
+++ src/app/components/webpages/scouting/field-scouting/field-scouting.component.ts
@@ -59,7 +59,7 @@ export class FieldScoutingComponent implements OnInit, OnDestroy {
   outstandingResponses: { id: number, team: number }[] = [];
 
   private stopwatchRun = false;
-  autoTime = 1;
+  autoTime = 15;
   stopwatchSecond = this.autoTime;
   stopwatchLoopCount = 0;
 
