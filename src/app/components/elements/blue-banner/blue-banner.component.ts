21d87eae96f1f48183c3c7145a950c47e884e8e5
Brandon <brandon@bduke.dev>
2025-04-06 19:43:52 -0400

    banner tweaks

--- src/app/components/elements/blue-banner/blue-banner.component.ts
+++ src/app/components/elements/blue-banner/blue-banner.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { AppSize, GeneralService } from '../../../services/general.service';
 
 @Component({
   selector: 'app-blue-banner',
@@ -6,7 +7,31 @@ import { Component, Input } from '@angular/core';
   templateUrl: './blue-banner.component.html',
   styleUrl: './blue-banner.component.scss'
 })
-export class BlueBannerComponent {
-  @Input() Title = '';
-  @Input() Event = '';
+export class BlueBannerComponent implements OnInit {
+  screenSize!: AppSize;
+  screenSizeSmall = AppSize.SM;
+
+  awards = [{ title: 'FIRST IMPACT AWARD', event: '2025 SMOKY MOUNTAINS REGIONAL' },
+  { title: 'WINNER', event: '2025 GREATER PITTSBURGH REGIONAL' },
+  { title: 'WINNER', event: '2016 WEST VIRGINIA ROBOTICS EXTREME' },
+  { title: 'WOODIE FLOWERS FINALIST AWARD', event: '2016 QUEEN CITY REGIONAL' },
+  { title: 'WINNER', event: '2015 SMOKY MOUNTAINS REGIONAL' },
+  { title: 'ENGINEERING INSPIRATION AWARD', event: '2014 SMOKY MOUNTAINS REGIONAL' },
+  { title: 'WINNER', event: '2011 PITTSBURGH REGIONAL' },
+  ]
+
+  constructor(private gs: GeneralService) { }
+
+  ngOnInit() {
+    this.setScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setScreenSize();
+  }
+
+  private setScreenSize(): void {
+    this.screenSize = this.gs.getScreenSize();
+  }
 }
