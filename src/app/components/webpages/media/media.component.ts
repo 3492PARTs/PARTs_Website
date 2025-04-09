9bf4129951c4c7b8be6181552ed85bc99f1a0570
Brandon Duke <brandon@bduke.dev>
2025-04-09 14:44:31 -0400

    new album covers

--- src/app/components/webpages/media/media.component.ts
+++ src/app/components/webpages/media/media.component.ts
@@ -4,12 +4,19 @@ import { HeaderComponent } from '../../atoms/header/header.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-media',
-    imports: [BoxComponent, HeaderComponent, RouterLink],
-    templateUrl: './media.component.html',
-    styleUrls: ['./media.component.scss']
+  selector: 'app-media',
+  imports: [BoxComponent, HeaderComponent, RouterLink],
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.scss']
 })
 export class MediaComponent implements OnInit {
+
+  albums = [
+    { routerLink: 'competition', imgSrc: '/sliderImages/slide1.jpg', label: 'competition' },
+    { routerLink: 'build-season', imgSrc: '/sliderImages/slide2.jpg', label: 'build season' },
+    { routerLink: 'community-outreach', imgSrc: '/sliderImages/slide3.jpg', label: 'community outreach' },
+    { routerLink: 'wallpapers', imgSrc: '/sliderImages/slide4.jpg', label: 'wallpapers' },
+  ];
 
   constructor() { }
 
