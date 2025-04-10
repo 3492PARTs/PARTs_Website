2cfcb8310e30ab9e040d6b0e9e1f319538067f25
Brandon Duke <brandon@bduke.dev>
2025-04-10 10:08:59 -0400

    album

--- src/app/components/elements/albums/albums.component.ts
+++ src/app/components/elements/albums/albums.component.ts
@@ -10,11 +10,24 @@ import { RouterLink } from '@angular/router';
 export class AlbumsComponent {
   @Input() Albums: Album[] = [];
 
+  toggleVisibility(event: Event) {
+    const clickedElement = event.currentTarget as HTMLElement;
+    const linksElement = clickedElement.children.item(1) as HTMLElement;
+    if (linksElement) {
+
+      if (['', '0', '0px'].includes(linksElement.style.height)) {
+        linksElement.style.height = `${linksElement.scrollHeight}px`;
+      }
+      else {
+        linksElement.style.height = '0px';
+      }
+    }
+  }
 }
 
 export class Album {
-  routerLink = '';
+  routerLink?: string = '';
   imgSrc = '';
   label = '';
-  links?: string[] = [];
+  links?: { label: string, href: string }[] = [];
 }
--- src/app/components/webpages/media/wallpapers/wallpapers.component.ts
+++ src/app/components/webpages/media/wallpapers/wallpapers.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ReturnCardComponent } from '../../../elements/return-card/return-card.component';
+import { Album, AlbumsComponent } from '../../../elements/albums/albums.component';
 
 @Component({
-    selector: 'app-wallpapers',
-    imports: [ReturnCardComponent],
-    templateUrl: './wallpapers.component.html',
-    styleUrls: ['../media.component.scss']
+  selector: 'app-wallpapers',
+  imports: [ReturnCardComponent, AlbumsComponent],
+  templateUrl: './wallpapers.component.html',
+  styleUrls: ['../media.component.scss']
 })
 export class WallpapersComponent implements OnInit {
+
+  albums: Album[] = [
+    { imgSrc: '/albumCovers/Wallpapers/4K2.png', label: '4K', links: [{ label: 'Desktop', href: 'https://photos.app.goo.gl/jJUwS9i9rYK5Cu97A' }, { label: 'Desktop2', href: 'https://photos.app.goo.gl/jJUwS9i9rYK5Cu97A' }] },
+    { routerLink: 'build-season', imgSrc: '/webImages/Albums/BuildSeason.JPG', label: 'build season' },
+    { routerLink: 'community-outreach', imgSrc: '/webImages/Albums/CommunityOutreach.JPG', label: 'community outreach' },
+    { routerLink: 'wallpapers', imgSrc: '/webImages/Albums/Wallpapers.jpg', label: 'wallpapers' },
+  ];
 
   constructor() { }
 
