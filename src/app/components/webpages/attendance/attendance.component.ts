b1d1427f948cf3c11c5215a06698ac21676a8ed6
Brandon <brandon@bduke.dev>
2025-10-23 15:24:31 -0400

    get attendance

--- src/app/components/webpages/attendance/attendance.component.ts
+++ src/app/components/webpages/attendance/attendance.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BoxComponent } from "../../atoms/box/box.component";
 import { ButtonComponent } from "../../atoms/button/button.component";
 import { APIService } from '../../../services/api.service';
@@ -7,20 +7,29 @@ import { AuthService } from '../../../services/auth.service';
 import { GeneralService, RetMessage } from '../../../services/general.service';
 import { Attendance } from '../../../models/attendance.models';
 import { Banner } from '../../../models/api.models';
+import { TableColType, TableComponent } from '../../atoms/table/table.component';
 
 @Component({
   selector: 'app-attendance',
-  imports: [BoxComponent, ButtonComponent],
+  imports: [BoxComponent, ButtonComponent, TableComponent],
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.scss'
 })
-export class AttendanceComponent {
+export class AttendanceComponent implements OnInit {
 
   private user: User | undefined = undefined;
+  attendance: Attendance[] = [];
+  attendanceTableCols: TableColType[] = [
+    { PropertyName: 'user.name', ColLabel: 'User' },
+    { PropertyName: 'time', ColLabel: 'Time' },
+  ];
 
   constructor(private api: APIService, private auth: AuthService, private gs: GeneralService) {
     auth.user.subscribe(u => this.user = u);
 
+  }
+  ngOnInit(): void {
+    this.get();
   }
 
   save(): void | null {
@@ -37,5 +46,11 @@ export class AttendanceComponent {
     }
     else
       this.gs.triggerError('Couldn\'t take attendance see a mentor.');
+  }
+
+  get(): void | null {
+    this.api.get(true, 'attendance/attendance/', undefined, (result: Attendance[]) => {
+      this.attendance = result;
+    });
   }
 }
