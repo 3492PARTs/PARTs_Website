df8aadb89cc1f7eef47765bf40ab17b0cea383ab
Brandon <brandon@bduke.dev>
2025-10-23 14:37:48 -0400

    save entry

--- src/app/components/webpages/attendance/attendance.component.ts
+++ src/app/components/webpages/attendance/attendance.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { BoxComponent } from "../../atoms/box/box.component";
+import { ButtonComponent } from "../../atoms/button/button.component";
+import { APIService } from '../../../services/api.service';
+import { User } from '../../../models/user.models';
+import { AuthService } from '../../../services/auth.service';
+import { GeneralService, RetMessage } from '../../../services/general.service';
+import { Attendance } from '../../../models/attendance.models';
+import { Banner } from '../../../models/api.models';
 
 @Component({
   selector: 'app-attendance',
-  imports: [],
+  imports: [BoxComponent, ButtonComponent],
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.scss'
 })
 export class AttendanceComponent {
 
+  private user: User | undefined = undefined;
+
+  constructor(private api: APIService, private auth: AuthService, private gs: GeneralService) {
+    auth.user.subscribe(u => this.user = u);
+
+  }
+
+  save(): void | null {
+    if (this.user) {
+      const a = new Attendance();
+      a.user = this.user;
+      this.api.post(true, 'attendance/attendance/',
+        a,
+        (result: any) => {
+          this.gs.addBanner(new Banner(0, (result as RetMessage).retMessage, 3500));
+        }, (err: any) => {
+          this.gs.triggerError(err);
+        });
+    }
+    else
+      this.gs.triggerError('Couldn\'t take attendance see a mentor.');
+  }
 }
---
+++ src/app/models/attendance.models.ts
@@ -0,0 +1,7 @@
+import { User } from "./user.models";
+
+export class Attendance {
+    id = NaN;
+    user = new User();
+    time = new Date()
+}
