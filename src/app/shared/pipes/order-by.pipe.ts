95877b11ad157ec2a35977e6d2b0b84c490d30c1
Brandon <brandon@bduke.dev>
2026-01-24 15:12:25 -0500

    fix filter/sort order on tables

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -1,16 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { getPropertyValue, strNoE } from '@app/core/utils/utils.functions';
 
 @Pipe({ name: 'OrderBy', standalone: true })
 export class OrderByPipe implements PipeTransform {
   transform(obj: any, OrderByProperty: string, reverseOrder: boolean): any {
-    obj.sort((a: { [x: string]: number; }, b: { [x: string]: number; }) => {
+    obj.sort((a: { [x: string]: any; }, b: { [x: string]: any; }) => {
+      const aValue = !strNoE(OrderByProperty) ? getPropertyValue(a, OrderByProperty) : a;
+      const bValue = !strNoE(OrderByProperty) ? getPropertyValue(b, OrderByProperty) : b;
       if (!reverseOrder) {
-        if (a[OrderByProperty] < b[OrderByProperty]) { return -1; }
-        if (a[OrderByProperty] > b[OrderByProperty]) { return 1; }
+        if (aValue < bValue) { return -1; }
+        if (aValue > bValue) { return 1; }
         return 0;
       } else {
-        if (a[OrderByProperty] < b[OrderByProperty]) { return 1; }
-        if (a[OrderByProperty] > b[OrderByProperty]) { return -1; }
+        if (aValue < bValue) { return 1; }
+        if (aValue > bValue) { return -1; }
         return 0;
       }
     });
