6278f3cdb1ff3ebd588bfed06017f07f18273ef3
Brandon Duke <brandon@bduke.dev>
2026-03-23 10:23:59 -0400

    let strat run report

--- src/app/scouting/components/strategizing/metrics/metrics.component.ts
+++ src/app/scouting/components/strategizing/metrics/metrics.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from "../../elements/dashboard/dashboard.component";
+import { APIService, ModalService, RetMessage, downloadFileAs } from '@app/core';
+import { ButtonComponent, FormElementGroupComponent } from '@app/shared';
 
 @Component({
   selector: 'app-metrics',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, FormElementGroupComponent, ButtonComponent],
   templateUrl: './metrics.component.html',
   styleUrls: ['./metrics.component.scss']
 })
 export class MetricsComponent {
 
+  constructor(private api: APIService, private modalService: ModalService) { }
+
+  runScoutingReport(): void {
+    this.api.get(true, 'scouting/admin/scouting-report/', undefined, (result: RetMessage) => {
+      //console.log(result);
+      downloadFileAs('ScoutReport.csv', result.retMessage, 'text/csv');
+    }, (err: any) => {
+      this.modalService.triggerError(err);
+    });
+  }
 }
