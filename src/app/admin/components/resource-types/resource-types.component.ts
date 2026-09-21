6a72477c40e213223f6242b3d25cda3be802c1e8
Brandon <brandon@bduke.dev>
2026-09-21 12:53:10 -0400

    mobile formatting

--- src/app/admin/components/resource-types/resource-types.component.ts
+++ src/app/admin/components/resource-types/resource-types.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthCallStates, AuthService } from '@app/auth/services/auth.service';
 import { Resource, ResourceType } from '@app/admin/models/resource.models';
 import { ResourceService } from '@app/admin/services/resource.service';
@@ -10,15 +10,20 @@ import { ButtonRibbonComponent } from '@app/shared/components/atoms/button-ribbo
 import { TableColType, TableComponent } from '@app/shared/components/atoms/table/table.component';
 import { ModalComponent } from '@app/shared/components/atoms/modal/modal.component';
 import { ResourceManagerComponent } from '@app/shared/components/elements/resource-manager/resource-manager.component';
-import { cloneObject } from '@app/core/utils/utils.functions';
+import { AppSize, cloneObject } from '@app/core/utils/utils.functions';
+import { GeneralService } from '@app/core/services/general.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-resource-types',
-    imports: [BoxComponent, FormElementComponent, FormComponent, ButtonComponent, ButtonRibbonComponent, TableComponent, ModalComponent, ResourceManagerComponent],
+    imports: [BoxComponent, FormElementComponent, FormComponent, ButtonComponent, ButtonRibbonComponent, TableComponent, ModalComponent, ResourceManagerComponent, CommonModule],
     templateUrl: './resource-types.component.html',
     styleUrls: ['./resource-types.component.scss']
 })
 export class ResourceTypesComponent implements OnInit {
+    appSize: AppSize = AppSize.SM;
+
+    breakPoint: AppSize = AppSize.LG;
 
     resourceTypesTableCols: TableColType[] = [
         { PropertyName: 'name', ColLabel: 'Name' },
@@ -39,7 +44,7 @@ export class ResourceTypesComponent implements OnInit {
     activeResource: Resource = new Resource();
     resourceModalVisible = false;
 
-    constructor(private authService: AuthService, private resourceService: ResourceService) { }
+    constructor(private authService: AuthService, private resourceService: ResourceService, private gs: GeneralService) { }
 
     ngOnInit(): void {
         this.authService.authInFlight.subscribe((r) => {
@@ -47,6 +52,12 @@ export class ResourceTypesComponent implements OnInit {
                 this.getResourceTypes();
             }
         });
+        this.appSize = this.gs.getAppSize();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.appSize = this.gs.getAppSize();
     }
 
     // RESOURCE TYPES ------------------------------------------------------
